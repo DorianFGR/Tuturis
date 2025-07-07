8cc92c71e049ed3e5e0ae2ffa8851bd1e66cb609
Dorian Figueiras <dorianfigueiras@icloud.com>
2025-07-07 20:32:14 +0200

    basic client / server config

--- client/main.cpp
+++ client/main.cpp
@@ -1,10 +1,34 @@
-#include <iostream>
 #include <boost/asio.hpp>
-#include <boost/system/error_code.hpp>
+#include <iostream>
+#include <array>
 
+using boost::asio::ip::tcp;
 
 int main() {
-    boost::asio::io_context io;
-    std::cout << "Boost ASIO is ready." << std::endl;
+    try {
+        boost::asio::io_context io;
+
+        tcp::resolver resolver(io);
+        auto endpoints = resolver.resolve("127.0.0.1", "2008");
+
+        tcp::socket socket(io);
+        boost::asio::connect(socket, endpoints);
+
+        std::array<char, 128> buf{};
+        boost::system::error_code error;
+
+        size_t len = socket.read_some(boost::asio::buffer(buf), error);
+        if (!error) {
+            std::cout << "Message received from server : ";
+            std::cout.write(buf.data(), len);
+            std::cout << std::endl;
+        } else {
+            std::cerr << "Error reading :  " << error.message() << std::endl;
+        }
+
+    } catch (std::exception& e) {
+        std::cerr << "Exception : " << e.what() << std::endl;
+    }
+
     return 0;
 }
--- server/main.cpp
+++ server/main.cpp
@@ -1,8 +1,36 @@
 #include <boost/asio.hpp>
-#include <boost/system/error_code.hpp>
+#include <iostream>
 
+using boost::asio::ip::tcp;
+int port = 2008;
 
-int main () {
+int main() {
+    try {
+        boost::asio::io_context io;
+
+        tcp::acceptor acceptor(io, tcp::endpoint(tcp::v4(), port));
+
+        std::cout << "Tuturis is listening on port : "<< port << std::endl;
+        std::cout << "Tuturis server is up waiting for clients" << std::endl;
+
+        for (;;) {
+            tcp::socket socket(io);
+            acceptor.accept(socket);
+
+            std::string client_ip = socket.remote_endpoint().address().to_string();
+            unsigned short port = socket.remote_endpoint().port();
+            std::cout << client_ip << ":" << port << " is now connected to the server" << std::endl;
+
+            std::string message = "Welcome on Tuturis server!\n";
+            boost::system::error_code ignored_error;
+            boost::asio::write(socket, boost::asio::buffer(message), ignored_error);
+
+            std::cout << "Message sent to client" << std::endl;
+        }
+
+    } catch (std::exception& e) {
+        std::cerr << "Error : " << e.what() << std::endl;
+    }
 
     return 0;
 }
