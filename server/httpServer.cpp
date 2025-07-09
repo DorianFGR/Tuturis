63b51fb30968d0b09e2310b8eeeaa5450198c428
Dorian Figueiras <dorianfigueiras@icloud.com>
2025-07-09 09:32:56 +0200

    controlPanel index page done

--- server/httpServer.cpp
+++ server/httpServer.cpp
@@ -17,6 +17,12 @@ std::string read_file(const std::string& path) {
     return { std::istreambuf_iterator<char>(file), std::istreambuf_iterator<char>() };
 }
 
+std::vector<char> read_binary_file(const std::string& path) {
+    std::ifstream file(path, std::ios::binary);
+    if (!file) return {};
+    return { std::istreambuf_iterator<char>(file), std::istreambuf_iterator<char>() };
+}
+
 void serve_page(tcp::socket socket) {
     try {
         beast::flat_buffer buffer;
@@ -35,10 +41,24 @@ void serve_page(tcp::socket socket) {
             res.result(http::status::ok);
             res.set(http::field::content_type, "text/html");
             res.body() = read_file("server/controlPanel/login.html");
-        }else if(req.method() == http::verb::get && req.target() == "/styles.css") {
+        }else if(req.method() == http::verb::get && req.target() == "/styles/login.css") {
             res.result(http::status::ok);
             res.set(http::field::content_type, "text/css");
-            res.body() = read_file("server/controlPanel/styles.css");
+            res.body() = read_file("server/controlPanel/styles/login.css");
+        }else if(req.method() == http::verb::get && req.target() == "/styles/board.css") {
+            res.result(http::status::ok);
+            res.set(http::field::content_type, "text/css");
+            res.body() = read_file("server/controlPanel/styles/board.css");
+        } else if (req.method() == http::verb::get && req.target() == "/img/user.webp") {
+            std::vector<char> image = read_binary_file("server/controlPanel/img/user.webp");
+
+            http::response<http::vector_body<char>> res_img{ http::status::ok, req.version() };
+            res_img.set(http::field::content_type, "image/webp");
+            res_img.content_length(image.size());
+            res_img.body() = std::move(image);
+            res_img.prepare_payload();
+            http::write(socket, res_img);
+            return;
         }else {
             res.result(http::status::not_found);
             res.set(http::field::content_type, "text/plain");
