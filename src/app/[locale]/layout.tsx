82114251b800e765341c7801938da85f7afad1de
Dorian Figueiras <dorianfigueiras@icloud.com>
2025-09-21 18:50:34 +0200

    Fix build crash due to previous commit

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -26,11 +26,11 @@ export const metadata: Metadata = {
 
 type Props = {
   children: React.ReactNode;
-  params: { locale: string };
+  params: Promise<{ locale: string }>;
 };
 
 export default async function LocaleLayout({ children, params }: Props) {
-  const { locale } = params;
+  const { locale } = await params;
 
   if (!hasLocale(routing.locales, locale)) {
     notFound();
